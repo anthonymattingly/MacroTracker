cb4e5d73965064ab6873a4bd98facf7b329bb0a2
anthonymattingly <amattingly100@gmail.com>
2018-02-10 18:26:48 -0500

    Updated create method in Controller class and updated create view.  Data not persisting still.

--- MacroTracker/Controllers/FoodController.cs
+++ MacroTracker/Controllers/FoodController.cs
@@ -1,7 +1,9 @@
 ﻿using MacroTracker.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,32 +24,64 @@ namespace MacroTracker.Controllers
         }
 
         [HttpGet]
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
             //ToDo--Make sure duplicate item not selected
-            
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
             return View(db.FoodExamples.ToList());
         }
 
         [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            var food = new Food();
+            return View("Create", food);
         }
 
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+       [ValidateAntiForgeryToken]
+        public ActionResult CreateFood([Bind(Include = "FoodName, FatGrams, CarbGrams, ProteinGrams")]Food food)
         {
+            using (var foodContext = new FoodContext()) { 
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
+                if (ModelState.IsValid)
+                {
+                        db.FoodExamples.Add(food);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                        //foodContext.FoodExamples.Add(food);
+                        //foodContext.SaveChanges();
+                        //return RedirectToAction("Index");
+                    }
+                }
+            catch (DataException /* dex */)
             {
-                return View();
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
             }
+            return View(food);
+        }
         }
 
         [HttpGet]
@@ -80,7 +114,7 @@ namespace MacroTracker.Controllers
 
         // POST: Foods/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult DeleteEntry(int id, FormCollection collection)
         {
             try
             {
